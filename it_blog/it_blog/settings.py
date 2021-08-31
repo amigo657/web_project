91e02eef4e5f1cfeefd20aedfb7e82006f833fff
Kolya <amigo.bro.657@gmail.com>
2021-08-31 21:52:08 +0300

    base and home templates

--- it_blog/it_blog/settings.py
+++ it_blog/it_blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'it_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "it_blog" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
