938cb12d1dabeea1de5467ac4a733791cb5d98e6
Kolya <amigo.bro.657@gmail.com>
2021-09-07 21:51:59 +0300

    Second page

--- it_blog/it_blog/urls.py
+++ it_blog/it_blog/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("home/", include("home.urls")),
+    path("blog/", include("blog.urls")),
     path("", lambda request: redirect("/home/")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
